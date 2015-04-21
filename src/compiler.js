e786718be6e78b00406d424f39556d67b846fc47
Steven Foote <smfoote@gmail.com>
2015-04-21 15:59:14 -0700

    Compiler support for attributes
    Including attributes with references. Still no support for bodies.

--- src/compiler.js
+++ src/compiler.js
@@ -11,7 +11,6 @@ let compiler = {
   compile(ast, name) {
     this.context = {
       tornadoBodiesIndex: 0,
-      elsWithRefs: [],
       htmlBodiesIndexes: [],
       htmlBodiesCount: -1,
       refCount: 0,
@@ -39,31 +38,50 @@ let compiler = {
   },
   step(node) {
     if (node[0] && this[node[0]]) {
-      this[node[0]](node);
+      return this[node[0]](node);
     }
   },
   walk(nodes = []) {
     nodes.forEach((n) => this.step(n));
     this.fragments = `${this.fragments}      return cache;
-  },\n`;
+    },`;
     this.renderers = `${this.renderers}      return root.frag;\n    },`;
   },
+  /**
+   * Walk through the attributes of an HTML element
+   */
+  walkAttrs(items = []) {
+    let res = [];
+    items.forEach((item) => {
+      res.push(this.step(item));
+    });
+    return res.join('+');
+  },
+  /**
+   * Walk through the contents of an HTML element
+   */
   walkContents(nodes = []) {
     var indexes = this.context.htmlBodiesIndexes;
     nodes.forEach((n) => {
       this.step(n);
-      console.log(`indexes before: ${JSON.stringify(indexes)}`);
       indexes[indexes.length-1]++;
-      console.log(`indexes after: ${JSON.stringify(indexes)}`);
     });
   },
   buildElementAttributes(attributes = []) {
     let attrs = '';
     let previousState = this.context.state;
+    let refCount = this.context.refCount;
     this.context.state = STATES.HTML_ATTRIBUTE;
-    for (let attr of attributes) {
-      attrs += `el${this.context.htmlBodiesCount}.setAttribute('${attr.attrName}', '${this.walk(attr.value)}');`;
-    }
+    attributes.forEach((attr) => {
+      let hasRef = attr.value.some(function(val) {
+        return val[0] === 'TORNADO_REFERENCE';
+      });
+      if (hasRef) {
+        this.renderers = `${this.renderers}      root.ref${refCount}.setAttribute('${attr.attrName}', ${this.walkAttrs(attr.value)});\n`;
+      } else {
+        this.fragments = `${this.fragments}      el${this.context.htmlBodiesCount}.setAttribute('${attr.attrName}', ${this.walkAttrs(attr.value)});\n`;
+      }
+    });
     this.context.state = previousState;
     return attrs;
   },
@@ -83,11 +101,14 @@ let compiler = {
     let indexes = this.context.htmlBodiesIndexes;
     let idx = indexes[indexes.length - 1]++;
     let refCount = this.context.refCount++;
+    let containerName = this.getElContainerName();
     if (this.context.state === STATES.HTML_BODY) {
-      let containerName = this.getElContainerName();
       this.fragments = `${this.fragments}      cache.ref${refCount} = ${containerName};
       ${this.getElContainerName()}.appendChild(document.createComment(''));\n`;
       this.renderers = `${this.renderers}      root.ref${refCount}.replaceChildAtIdx(${idx}, document.createTextNode(td.get(c, ${JSON.stringify(node[1].key)})));\n`;
+    } else if (this.context.state === STATES.HTML_ATTRIBUTE) {
+      this.fragments = `${this.fragments}      cache.ref${refCount} = ${containerName};\n`;
+      return `td.get(c, ${JSON.stringify(node[1].key)})`;
     }
   },
   HTML_ELEMENT(node) {
@@ -96,9 +117,8 @@ let compiler = {
     this.context.state = STATES.HTML_BODY;
     this.context.htmlBodiesIndexes.push(0);
     let count = ++this.context.htmlBodiesCount;
-    console.log(nodeInfo.key);
-    console.log(JSON.stringify(this.context.htmlBodiesIndexes));
-    this.fragments = `${this.fragments}      var el${count} = document.createElement("${nodeInfo.key}");${this.buildElementAttributes(nodeInfo.attributes)}\n`;
+    this.fragments = `${this.fragments}      var el${count} = document.createElement("${nodeInfo.key}");\n`;
+    this.buildElementAttributes(nodeInfo.attributes);
     this.walkContents(nodeContents);
     this.context.htmlBodiesIndexes.pop();
     this.context.htmlBodiesCount--;
@@ -106,7 +126,7 @@ let compiler = {
   },
   PLAIN_TEXT(node) {
     if (this.context.state === STATES.HTML_ATTRIBUTE) {
-      return node[1];
+      return '\'' + node[1] + '\'';
     } else if (this.context.state === STATES.HTML_BODY) {
       this.fragments = `${this.fragments}      ${this.getElContainerName()}.appendChild(document.createTextNode('${node[1]}'));\n`;
     }
