772e814d868a76b5bd925ccfa576b1765686a327
Steven Foote <smfoote@gmail.com>
2015-04-30 10:50:07 -0700

    Fix whitespace in runtime.js

--- src/runtime.js
+++ src/runtime.js
@@ -28,84 +28,85 @@ let tornado = {
    * @param {Array} path An array of key names.
    * @return {*} The value at the end of the path, or an empty string.
    */
-   get(context, path) {
-     let pathLength = path.length;
-     let newContext;
-     if (pathLength === 1) {
-       return context[path.pop()] || '';
-     } else if (!pathLength || pathLength < 0) {
-       return '';
-     }
-     newContext = context[path.pop()];
-     if (this.util.isObject(newContext)) {
-       return this.get(newContext, path);
-     }
-     return '';
-   },
+  get(context, path) {
+    let pathLength = path.length;
+    let newContext;
+    if (pathLength === 1) {
+      return context[path.pop()] || '';
+    } else if (!pathLength || pathLength < 0) {
+      return '';
+    }
+    newContext = context[path.pop()];
+    if (this.util.isObject(newContext)) {
+      return this.get(newContext, path);
+    }
+    return '';
+  },
 
-   /**
-    * Replace a DOM node (text or element or comment or whatever) with a given
-    * set of indexes from the root element. If a node is not found in the path of indexes, the
-    * function does nothing.
-    * @example
-    *
-    * Root element
-    * ============
-    * [documentFragment]<div>This is some text. <span>And a span.</span></div>[/documentFragment]
-    *
-    * Method call
-    * ===========
-    * // The call below would replace the text node "And a span." with `myNewNode`.
-    * td.replaceChildAtIdx(root, [0,1,0], myNewNode);
-    * // [
-    *      0 **the outer div is the 0th child of the root**,
-    *      1 **the span is the 1st child of the div**,
-    *      0 **the text node is the 0th child of the span**
-    *    ]
-    *
-    * @param {DocumentFragment} root When used within a compiled template, this is always a document
-    * fragment, but it could be a regular HTML Element
-    * @param {Array} indexPath An array of indexes leading to the node to be replaced
-    * @param {Node} newNode The node to take the place of the replaced node.
-    */
-    replaceChildAtIdxPath(root, indexPath, newNode) {
-      let finalIndex = indexPath.pop();
-      let parentNode = this.getNodeAtIdxPath(root, indexPath);
-      let oldNode;
-      if (parentNode) {
-        oldNode = this.getNodeAtIdxPath(parentNode, [finalIndex]);
-      } else {
-        return;
-      }
-      if (oldNode) {
-        parentNode.replaceChild(newNode, oldNode);
-      } else if (finalIndex >= parentNode.childNodes.length) {
-        parentNode.appendChild(newNode);
-      }
-    },
+  /**
+   * Replace a DOM node (text or element or comment or whatever) with a given
+   * set of indexes from the root element. If a node is not found in the path of indexes, the
+   * function does nothing.
+   * @example
+   *
+   * Root element
+   * ============
+   * [documentFragment]<div>This is some text. <span>And a span.</span></div>[/documentFragment]
+   *
+   * Method call
+   * ===========
+   * // The call below would replace the text node "And a span." with `myNewNode`.
+   * td.replaceChildAtIdx(root, [0,1,0], myNewNode);
+   * // [
+   *      0 **the outer div is the 0th child of the root**,
+   *      1 **the span is the 1st child of the div**,
+   *      0 **the text node is the 0th child of the span**
+   *    ]
+   *
+   * @param {DocumentFragment} root When used within a compiled template, this is always a document
+   * fragment, but it could be a regular HTML Element
+   * @param {Array} indexPath An array of indexes leading to the node to be replaced
+   * @param {Node} newNode The node to take the place of the replaced node.
+   */
+  replaceChildAtIdxPath(root, indexPath, newNode) {
+    let finalIndex = indexPath.pop();
+    let parentNode = this.getNodeAtIdxPath(root, indexPath);
+    let oldNode;
+    if (parentNode) {
+      oldNode = this.getNodeAtIdxPath(parentNode, [finalIndex]);
+    } else {
+      return;
+    }
+    if (oldNode) {
+      parentNode.replaceChild(newNode, oldNode);
+    } else if (finalIndex >= parentNode.childNodes.length) {
+      parentNode.appendChild(newNode);
+    }
+  },
 
-    /**
-     * Tornado-specific truthiness (based on dust.isEmpty). 0 is truthy, empty array is falsy,
-     * everything else matches JavaScript truthiness.
-     * @param {*} val The value to be checked for existence.
-     * @return {Boolean}
-     */
-     exists(val) {
-       if (val === 0) {
-         return true;
-       }
-       if (Array.isArray(val) && !val.length) {
-         return false;
-       }
-       return !!val;
-     },
- /**
-  * Within a given HTML node, find the node at the given index path. See replaceChildAtIdxPath
-  * for more details.
-  * @param {HTMLNode} root The parent node
-  * @param {Array} indexPath The path of indexes to the node being searched for.
-  * @param {HTMLNode|Boolean} The HTML Node if it is found, or `false`
-  */
+  /**
+   * Tornado-specific truthiness (based on dust.isEmpty). 0 is truthy, empty array is falsy,
+   * everything else matches JavaScript truthiness.
+   * @param {*} val The value to be checked for existence.
+   * @return {Boolean}
+   */
+  exists(val) {
+    if (val === 0) {
+      return true;
+    }
+    if (Array.isArray(val) && !val.length) {
+      return false;
+    }
+    return !!val;
+  },
+
+  /**
+   * Within a given HTML node, find the node at the given index path. See replaceChildAtIdxPath
+   * for more details.
+   * @param {HTMLNode} root The parent node
+   * @param {Array} indexPath The path of indexes to the node being searched for.
+   * @param {HTMLNode|Boolean} The HTML Node if it is found, or `false`
+   */
   getNodeAtIdxPath(root, indexPath) {
     let nextIdx;
 
@@ -126,16 +127,16 @@ let tornado = {
     return intermediateNode || false;
   },
 
-   util: {
-     /**
-      * Determine if a value is an object
-      * @param {*} val The value in question
-      * @return {Boolean}
-      */
-     isObject(val) {
-       return typeof val === 'object' && val !== null;
-     }
-   }
+  util: {
+    /**
+     * Determine if a value is an object
+     * @param {*} val The value in question
+     * @return {Boolean}
+     */
+    isObject(val) {
+      return typeof val === 'object' && val !== null;
+    }
+  }
 }
 
 module.exports = tornado;
