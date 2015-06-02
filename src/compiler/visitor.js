9440c025610ed5a5b055940a1c5aa1a50b32aaab
Jimmy Chan <jimmyhchan@users.noreply.github.com>
2015-06-02 10:36:20 -0700

    walk the ast in the vistor. combine bodies in the AST

--- src/compiler/visitor.js
+++ src/compiler/visitor.js
@@ -7,10 +7,38 @@ let visitor = {
   build(fns) {
     let step = function(node) {
       let type = node[0];
+      let extraArgs = Array.prototype.slice.call(arguments, 1);
+      let output;
+      // TODO: add enter and leave logic so node handlers can set up the context for the children
       if (type && fns[type]) {
-        return fns[type].apply(null, arguments);
+        output = fns[type].apply(null, arguments);
       }
+
+      // also walk the child nodes for those nodes with children
+      switch (type) {
+        case 'HTML_ELEMENT':
+          if (node[1].tag_contents) {
+            walk.apply(null, [node[1].tag_contents].concat(extraArgs));
+          }
+          break;
+        case 'TORNADO_BODY':
+          if (node[1].bodies) {
+            walk.apply(null, [node[1].bodies].concat(extraArgs));
+          }
+          if (node[1].params) {
+            walk.apply(null, [node[1].params].concat(extraArgs));
+          }
+          break;
+        case 'TORNADO_PARTIAL':
+          if (node[1].params) {
+            walk.apply(null, [node[1].params].concat(extraArgs));
+          }
+          break;
+      }
+
+      return output;
     };
+
     let walk = function(nodes=[]) {
       let extraArgs = Array.prototype.slice.call(arguments, 1);
       nodes.forEach((n) => {
