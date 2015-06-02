5285e5c314e06b554ca7e8e69e509c38b934c11f
Steven Foote <smfoote@gmail.com>
2015-06-02 12:43:15 -0600

    Add render perf to benchmark

---
+++ benchmark/benchmark.js
@@ -0,0 +1,14 @@
+var parser = require('../../dist/parser'),
+    compiler = require('../../dist/compiler'),
+    td = require('../../dist/runtime');
+
+window.td = td;
+var button = document.querySelector('#run');
+var tornadoContainer = document.querySelector('#tornado-container');
+var tornadoResults = document.querySelector('#tornado-results');
+var dustContainer = document.querySelector('#dust-container');
+var dustResults = document.querySelector('#dust-results');
+
+button.addEventListener('click', function() {
+
+});
