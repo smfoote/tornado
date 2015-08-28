54fda11224d37ac2afa7f2a59bf3c4015d2ebca1
Jimmy Chan <jimmyhchan@users.noreply.github.com>
2015-08-28 11:00:03 -0700

    start of the writer

--- dist/compiler/extensions/postprocess.js
+++ dist/compiler/extensions/postprocess.js
@@ -4,6 +4,69 @@ var _interopRequire = function (obj) { return obj && obj.__esModule ? obj["defau
 
 var util = _interopRequire(require("../utils/builder"));
 
+// helper method for recursively writing elements
+var writeElements = (function (_writeElements) {
+  var _writeElementsWrapper = function writeElements(_x, _x2, _x3, _x4) {
+    return _writeElements.apply(this, arguments);
+  };
+
+  _writeElementsWrapper.toString = function () {
+    return _writeElements.toString();
+  };
+
+  return _writeElementsWrapper;
+})(function (indexes, elements, parent, out) {
+  parent = parent ? "el" + parent : "frag";
+  indexes.forEach(function (i) {
+    var el = elements[i];
+    if (el.type === "placeholder" || el.type === "plaintext") {
+      out.push("var el" + i + " = td.createTextNode('" + el.type + "');\n");
+    } else {
+      out.push("var el" + i + " = td.createElement('" + el.type + "');\n");
+    }
+    out.push("" + parent + ".appendChild(el" + i + ");\n");
+    // recurse over the children
+    if (el.elements) {
+      writeElements(el.elements, elements, parent, out);
+    }
+  });
+});
+
+var writeFragments = function writeFragments(results) {
+  var fragments = results.state.entities.fragments,
+      elements = results.state.entities.elements;
+
+  // initialize the results fragment
+  results.code.fragments = [];
+
+  fragments.forEach(function (f, idx) {
+    var codeFragments = [];
+    // add method header
+    codeFragments.push("f" + idx + ": function() {\n      var res = {};\n      var frag = td." + util.getTdMethodName("createDocumentFragment") + "();\n      res.frag = frag;\n");
+    // add all elements of this fragment
+    writeElements(f.elements, elements, null, codeFragments);
+    // add method footer
+    codeFragments.push("}");
+    results.code.fragments.push(codeFragments.join(""));
+  });
+};
+
+var writeBodys = function writeBodys(results) {
+  var bodys = results.state.entities.bodys;
+  // initialize the results renderer
+  results.code.renderers = [];
+  bodys.forEach(function (b, idx) {
+    var codeRenderer = [];
+    // add method header
+    codeRenderer.push("r" + idx + ": function() {\n      var root = this.f" + b.fragment + ";\n");
+    // add all mains
+    // add all alternate bodies
+    // add method footer
+    codeRenderer.push("return root.frag\n}");
+    results.code.renderers.push(codeRenderer.join(""));
+  });
+};
+
 var flush = function flush(results) {
   var templateVars = [];
   if (results.extensions && results.extensions.templateVars) {
@@ -14,6 +77,8 @@ var flush = function flush(results) {
 
 var postprocess = function postprocess(results) {
   if (results) {
+    writeFragments(results);
+    writeBodys(results);
     flush(results);
   }
 };
--- src/compiler/extensions/postprocess.js
+++ src/compiler/extensions/postprocess.js
@@ -1,5 +1,63 @@
 import util from '../utils/builder';
 
+// helper method for recursively writing elements
+let writeElements = function(indexes, elements, parent, out) {
+  parent = parent ? 'el' + parent : 'frag';
+  indexes.forEach(function(i) {
+    let el = elements[i];
+    if (el.type === 'placeholder' || el.type === 'plaintext') {
+      out.push(`var el${i} = td.createTextNode('${el.type}');\n`);
+    } else {
+      out.push(`var el${i} = td.createElement('${el.type}');\n`);
+    }
+    out.push(`${parent}.appendChild(el${i});\n`);
+    // recurse over the children
+    if (el.elements) {
+      writeElements(el.elements, elements, parent, out);
+    }
+  });
+};
+
+let writeFragments = function(results) {
+  let fragments = results.state.entities.fragments,
+      elements = results.state.entities.elements;
+
+  // initialize the results fragment
+  results.code.fragments = [];
+
+  fragments.forEach(function(f, idx) {
+    let codeFragments = [];
+    // add method header
+    codeFragments.push(`f${idx}: function() {
+      var res = {};
+      var frag = td.${util.getTdMethodName('createDocumentFragment')}();
+      res.frag = frag;\n`);
+    // add all elements of this fragment
+    writeElements(f.elements, elements, null, codeFragments);
+    // add method footer
+    codeFragments.push('}');
+    results.code.fragments.push(codeFragments.join(''));
+  });
+};
+
+let writeBodys = function(results) {
+  let bodys = results.state.entities.bodys;
+  // initialize the results renderer
+  results.code.renderers = [];
+  bodys.forEach(function(b, idx) {
+    let codeRenderer = [];
+    // add method header
+    codeRenderer.push(`r${idx}: function() {
+      var root = this.f${b.fragment};\n`);
+    // add all mains
+    // add all alternate bodies
+    // add method footer
+    codeRenderer.push('return root.frag;\n}');
+    results.code.renderers.push(codeRenderer.join(''));
+
+  });
+};
+
 let flush = function(results) {
   let templateVars = [];
   if (results.extensions && results.extensions.templateVars) {
@@ -20,6 +78,8 @@ var frags = {},
 
 let postprocess = function(results) {
   if (results) {
+    writeFragments(results);
+    writeBodys(results);
     flush(results);
   }
 };
