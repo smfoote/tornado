76eab1e6af839a480517883b8bd5a135b2d1d4f4
Steven Foote <smfoote@gmail.com>
2015-09-04 15:14:14 -0600

    Convert `section` to be a helper
    `{#hello}yellow{/hello}` and `{@section key="hello"}yellow{/section}`
    are now functionally the same
    
    Depends on #121

--- dist/builtinHelpers.js
+++ dist/builtinHelpers.js
@@ -2,15 +2,13 @@
 
 var _interopRequire = function (obj) { return obj && obj.__esModule ? obj["default"] : obj; };
 
-/*eslint no-debugger:0 */
-
 var util = _interopRequire(require("./util"));
 
 /**
  * Check if a value is truthy. If the value is a promise, wait until the promise resolves,
  * then check if the resolved value is truthy.
  */
-function exists(context, params, bodies, td) {
+var exists = function exists(context, params, bodies, td) {
   var key = params.key.split(".");
   var val = td.get(context, key);
   if (util.isPromise(val)) {
@@ -36,22 +34,82 @@ function exists(context, params, bodies, td) {
       return bodies["else"](context);
     }
   }
-}
+};
 
 /**
  * Inverse of exists
  */
-function notExists(context, params, bodies, td) {
+var notExists = function notExists(context, params, bodies, td) {
   var mainBody = bodies.main;
   var elseBody = bodies["else"];
   bodies.main = elseBody;
   bodies["else"] = mainBody;
   return exists(context, params, bodies, td);
-}
+};
+
+/**
+ * Break out the logic of whether the rendering context of a section is an Array
+ * @param {Function} body The appropriate body rendering function to be rendered with `val`.
+ * @param {*} ctx The context to be used to render the body
+ * @return {DocumentFragment}
+ */
+var sectionResult = function sectionResult(body, ctx, td) {
+  if (!body) {
+    return "";
+  }
+  if (Array.isArray(ctx)) {
+    var frag = td.createDocumentFragment();
+    td.helperContext.set("len", ctx.length);
+    for (var i = 0, item = undefined; item = ctx[i]; i++) {
+      td.helperContext.set("idx", i);
+      frag.appendChild(body(item));
+    }
+    return frag;
+  } else {
+    return body(ctx);
+  }
+};
+
+/**
+ * Check for truthiness in the same way the exists helper does. If truthy, render the main
+ * body with using the result of `td.get(params.key.split('.')` as the context (if the result is
+ * an array, loop through the array and render the main body for each value in the array). If
+ * falsy, optionally render the else body using `context`. Handle promises the way the exists
+ * helper does.
+ */
+var section = function section(context, params, bodies, td) {
+  var key = params.key.split(".");
+  var val = td.get(context, key);
+  var body = undefined,
+      ctx = undefined;
+  if (util.isPromise(val)) {
+    return val.then(function (data) {
+      if (util.isTruthy(data)) {
+        return sectionResult(bodies.main, data, td);
+      } else {
+        return sectionResult(bodies["else"], context, td);
+      }
+    })["catch"](function () {
+      return sectionResult(bodies["else"], context, td);
+    });
+  } else {
+    if (util.isTruthy(val)) {
+      body = bodies.main;
+      ctx = val;
+    } else {
+      body = bodies["else"];
+      ctx = context;
+    }
+    var res = undefined;
+    res = sectionResult(body, ctx, td);
+    return res;
+  }
+};
 
 var helpers = {
   exists: exists,
-  notExists: notExists
+  notExists: notExists,
+  section: section
 };
 
 module.exports = helpers;
--- src/builtinHelpers.js
+++ src/builtinHelpers.js
@@ -1,12 +1,10 @@
-/*eslint no-debugger:0 */
-
 import util from './util';
 
 /**
  * Check if a value is truthy. If the value is a promise, wait until the promise resolves,
  * then check if the resolved value is truthy.
  */
-function exists(context, params, bodies, td) {
+let exists = function exists(context, params, bodies, td) {
   let key = params.key.split('.');
   let val = td.get(context, key);
   if (util.isPromise(val)) {
@@ -32,22 +30,82 @@ function exists(context, params, bodies, td) {
       return bodies.else(context);
     }
   }
-}
+};
 
 /**
  * Inverse of exists
  */
-function notExists(context, params, bodies, td) {
+let notExists = function notExists(context, params, bodies, td) {
   let mainBody = bodies.main;
   let elseBody = bodies.else;
   bodies.main = elseBody;
   bodies.else = mainBody;
   return exists(context, params, bodies, td);
-}
+};
+
+
+/**
+ * Break out the logic of whether the rendering context of a section is an Array
+ * @param {Function} body The appropriate body rendering function to be rendered with `val`.
+ * @param {*} ctx The context to be used to render the body
+ * @return {DocumentFragment}
+ */
+let sectionResult = function(body, ctx, td) {
+  if (!body) {
+    return '';
+  }
+  if (Array.isArray(ctx)) {
+    let frag = td.createDocumentFragment();
+    td.helperContext.set('len', ctx.length);
+    for (let i = 0, item; (item = ctx[i]); i++) {
+      td.helperContext.set('idx', i);
+      frag.appendChild(body(item));
+    }
+    return frag;
+  } else {
+    return body(ctx);
+  }
+};
+
+/**
+ * Check for truthiness in the same way the exists helper does. If truthy, render the main
+ * body with using the result of `td.get(params.key.split('.')` as the context (if the result is
+ * an array, loop through the array and render the main body for each value in the array). If
+ * falsy, optionally render the else body using `context`. Handle promises the way the exists
+ * helper does.
+ */
+let section = function section(context, params, bodies, td) {
+  let key = params.key.split('.');
+  let val = td.get(context, key);
+  let body, ctx;
+  if (util.isPromise(val)) {
+    return val.then(data => {
+      if (util.isTruthy(data)) {
+        return sectionResult(bodies.main, data, td);
+      } else {
+        return sectionResult(bodies.else, context, td);
+      }
+    }).catch(() => {
+      return sectionResult(bodies.else, context, td);
+    });
+  } else {
+    if (util.isTruthy(val)) {
+      body = bodies.main;
+      ctx = val;
+    } else {
+      body = bodies.else;
+      ctx = context;
+    }
+    let res;
+    res = sectionResult(body, ctx, td);
+    return res;
+  }
+};
 
 let helpers = {
   exists,
-  notExists
+  notExists,
+  section
 };
 
 export default helpers;
